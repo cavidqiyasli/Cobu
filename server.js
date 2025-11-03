fccb3dfe90fcda7a33d28f28812e86b19b33802a
cavidqiyasli <qiyaslicavid81@gmail.com>
2025-11-03 16:16:28 +0545

    Update server.js

--- server.js
+++ server.js
@@ -4,20 +4,25 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 let players = {};
 
 io.on('connection', socket => {
   console.log('Yeni oyunçu qoşuldu:', socket.id);
 
-  // Yeni oyunçu əlavə olunur
+  // 🔹 Yeni oyunçu əlavə olunur
   socket.on('newPlayer', data => {
     players[socket.id] = { x: data.x, y: data.y };
     socket.broadcast.emit('playerJoined', { id: socket.id, x: data.x, y: data.y });
   });
 
-  // Oyunçu hərəkət edir
+  // 🔹 Oyunçu hərəkət edir
   socket.on('move', data => {
     if (players[socket.id]) {
       players[socket.id].x = data.x;
@@ -25,7 +30,18 @@ io.on('connection', socket => {
     }
   });
 
-  // Oyunçu çıxır
+  // 🔹 Oyunçu atəş açır
+  socket.on('shoot', data => {
+    // Bütün oyunçulara bu atış hadisəsini göndər
+    io.emit('playerShoot', {
+      id: socket.id,
+      x: data.x,
+      y: data.y,
+      angle: data.angle
+    });
+  });
+
+  // 🔹 Oyunçu ayrılır
   socket.on('disconnect', () => {
     console.log('Oyunçu ayrıldı:', socket.id);
     delete players[socket.id];
@@ -33,9 +49,10 @@ io.on('connection', socket => {
   });
 });
 
-// 🔹 Bütün oyunçulara hər 50ms-də mövqeləri göndər
+// 🔹 Hər 50ms-də bütün oyunçuların mövqeyini göndər
 setInterval(() => {
   io.emit('positions', players);
 }, 50);
 
+// 🔹 Serveri işə sal
 server.listen(10000, () => console.log('Server işə düşdü, port: 10000'));
